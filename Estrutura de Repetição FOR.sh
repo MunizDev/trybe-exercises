3411ccbe9704c2bf3a832e327afe02beb94274fc
Muniz <vinicius.muniz.marin@gmail.com>
2020-08-17 19:26:55 -0300

    Permissões de arquivos alteradas

---
+++ Estrutura de Repetição FOR.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#LISTA QUE SERÁ QUEBRADA
+LISTA="shell script usando estrutura repetição for terminal"
+
+#N VARIÁVEL DE REPETIÇÃO QUE RECEBE OS VALORES CONTIDOS EM $LISTA.
+for N in $LISTA
+do
+echo $N
+done
+
+#ESTRUTURA BÁSICA
