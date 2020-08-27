cfd5fb9ea8f41628d829f20c19edac4389a31e6c
Muniz <vinicius.muniz.marin@gmail.com>
2020-08-27 18:22:01 -0300

    Programas 1, 2, 3 feitos e testados

---
+++ modulo_1/bloco_4/dia_1/programa_2.js
@@ -0,0 +1,14 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let a = 5;
+let b = 8;
+
+if (a > b) {
+    console.log(a);
+}
+else if (b > a) {
+    console.log(b);
+}
+else {
+    console.log("os numeros sao iguais")
+}
