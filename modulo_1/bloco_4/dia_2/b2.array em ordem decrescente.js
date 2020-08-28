9db193f6d7595c1b888da45324cba42d4f08b971
Muniz <vinicius.muniz.marin@gmail.com>
2020-08-28 20:17:15 -0300

    adicionado outra forma de resolver os exercicios b1 e b2

--- modulo_1/bloco_4/dia_2/b2.array em ordem decrescente.js
+++ modulo_1/bloco_4/dia_2/b2.array em ordem decrescente.js
@@ -2,8 +2,21 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-numbers.sort(function (a, b) {
-    return b - a;
-});
+for (let i = 1; i < numbers.length; i++) {
+    for (let j = 0; j < i; j++) {
+        if (numbers[i] > numbers[j]) {
+            let position = numbers[i];
+
+            numbers[i] = numbers[j];
+            numbers[j] = position;
+        }
+    }
+}
 
 console.log(numbers);
+
+// numbers.sort(function (a, b) {
+//     return b - a;
+// });
+
+// console.log(numbers);
