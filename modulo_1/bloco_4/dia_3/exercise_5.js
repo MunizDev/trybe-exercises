078362ee25b3d79af1e56aa6d0e8a14eda664614
Muniz <vinicius.muniz.marin@gmail.com>
2020-08-31 19:02:53 -0300

    Exercício 5 feito utilizado if e for

--- modulo_1/bloco_4/dia_3/exercise_5.js
+++ modulo_1/bloco_4/dia_3/exercise_5.js
@@ -7,10 +7,38 @@ para n=7 percebemos que os espaços do meio seguem na sguinte sequência: x-2 es
 
 então podemos utilizar o if para alterar a quantidade de * colocados.
 */
-
-if ( x>1 && x<n ){
-    //coloca a sequência de espaços.
+let n = 15;
+let linha = "";
+let espaco = "";
+let espacoMeio = "";
+if (n > 1 && n % 2 === 1) {
+    //altera a quantidade de asteristicos em cada linha
+    for (let x = 1; x <= n; x += 2) {
+        linha = "";
+        espaco = "";
+        espacoMeio = "";
+        //inclui os espacos em cada linha em função de n e x
+        for (let j = 1; j <= (n - x) / 2; j += 1) {
+            espaco += " ";
+        }
+        //inclui os os ateristicos em cada linha em função de x
+        if (x > 1 && x < n) {
+            //coloca a sequência de espaços no meio.
+            for (let i = 1; i <= x - 2; i += 1) {
+                espacoMeio += " ";
+            }
+            linha += "*" + espacoMeio + "*";
+        }
+        else {
+            //mantém do mesmo jeito da piramide do exercício 4.
+            for (let i = 1; i <= x; i += 1) {
+                linha += "*";
+            }
+        }
+        //impressao de cadqa linha
+        console.log(espaco + linha);
+    }
 }
 else {
-    //mantém do mesmo jeito da piramide do exercício 4.
+    console.log('Aviso: o n precisa ser ímpar e maior que 1')
 }
