34043a89771961ddf24e1f6bed3272b6973d2357
Muniz <vinicius.muniz.marin@gmail.com>
2020-09-04 21:54:31 -0300

    2.5 Refeito exercício e testado, foi utilizado for e if

--- modulo_1/bloco_4/dia_4/2.5.retorna o inteiro que mais se repete.js
+++ modulo_1/bloco_4/dia_4/2.5.retorna o inteiro que mais se repete.js
@@ -1,47 +1,27 @@
 // Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
 
-/*
-1.Podemos começar pensando em comparar cada inteiro do array com os demais.
-2.Podemos criar uma função que faz essa comparação e nos retorna o número de repetições na variavel numeroRepeticoes.
-3 Depois temos que criar outra função que compara o número de repetições e retorne o indice do elemento que possui o valor da variavel numeroRepeticoes maior.
-*/
 
-//com um elemento do array escolhido a função percorre os demais para contar o número de repetições.
-function numeroRepeticoes(array, indice) {
-    //arrayteste, 1
-    let numeroRepeticoes = 0;
-    for (let i = 0; i < array.length; i += 1) {
-        if (array[indice] === array[i]) {
-            numeroRepeticoes += 1;
+//vamos usar uma variavel chamada ocorrencia para contar as vezes de repetição de cada elemento do array dado.
+function inteiroMaisRepete(arrayInteiros) {
+    let ocorrencia = 0;
+    let ocorrenciaMaior = 0;
+    let maior = arrayInteiros[0];
+    //numero i
+    for (let i in arrayInteiros) {
+        ocorrencia = 0;
+        //percorrendo os outros elementos
+        for (let j = 0; j < arrayInteiros.length; j += 1) {
+            if (arrayInteiros[i] === arrayInteiros[j]) {
+                ocorrencia += 1;
+            }
+        }
+        if (ocorrencia > ocorrenciaMaior) {
+            ocorrenciaMaior = ocorrencia;
+            maior = arrayInteiros[i];
         }
     }
-    return numeroRepeticoes;
+    return maior;
 }
 
-//compara se o elemento seguinte possui mais repetições se sim, armazena esse elemento na variavel elementoMaisRepete e depois de percorrer todo array retorna essa variavel.
-function comparaElementosArray(arrayEscolhido) {
-    let elementoMaisRepete = arrayEscolhido[0];
-    for (let j = 0; j < arrayEscolhido.length - 1; j += 1) {
-        if (
-            numeroRepeticoes(arrayEscolhido, j + 1) > numeroRepeticoes(arrayEscolhido, j)
-        ) {
-            elementoMaisRepete = arrayEscolhido[j + 1];
-        }
-    }
-    return elementoMaisRepete;
-}
-
-let arrayTeste = [2, 3, 2, 5, 8, 2, 3];
-console.log(comparaElementosArray(arrayTeste));
-
-
-
-
-
-
-//arrumar função comparaElementosArray
-/*
-podemos montar um array paralelo com a contagem de repetições de cada inteiro...
-deixando os indices paralelos dos dois arrays (contagem de repeticao, arrayEscolhido)
-podemos achar o maior número no array contagem de repetição e usar esse infice para retornar o valor do arrayEscolhido que mais se repete
-*/
+let arrayTeste = [0, 3, 2, 5, 8, 2, 3, 2];
+console.log(inteiroMaisRepete(arrayTeste));
