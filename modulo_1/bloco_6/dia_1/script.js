dd34ddaffa12c6825dbc841d5692ec8772792017
Muniz <vinicius.muniz.marin@gmail.com>
2020-09-17 20:38:22 -0300

    criado o primeiro fieldset

---
+++ modulo_1/bloco_6/dia_1/script.js
@@ -0,0 +1,12 @@
+let valuesStates = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'EX'];
+
+let optionsStates = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Estrangeiro'];
+
+let comboBox = document.getElementById('states');
+
+for (let index = 0; index < optionsStates.length; index += 1) {
+    let optionTag = document.createElement('option');
+    optionTag.value = valuesStates[index];
+    optionTag.innerText = optionsStates[index];
+    comboBox.appendChild(optionTag);
+}
