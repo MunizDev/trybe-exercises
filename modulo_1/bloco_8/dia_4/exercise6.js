e9ee980960a9218e31294805642d1887c536034b
Muniz <vinicius.muniz.marin@gmail.com>
2020-10-08 19:23:46 -0300

    exercise6.js corrigido

--- modulo_1/bloco_8/dia_4/exercise6.js
+++ modulo_1/bloco_8/dia_4/exercise6.js
@@ -1,11 +1,19 @@
-const assert = require('assert')
+const assert = require('assert');
 
-const palio = ["Palio", "Fiat", 2019]
-const shelbyCobra = ["Shelby Cobra", "Ford", 1963]
-const chiron = ["Chiron", "Bugatti", 2016]
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
 
-const toObject = ([name, brand, year]) => ({ name, brand, year })
+const toObject = ([name, brand, year]) => ({ name, brand, year });
 
-assert.deepEqual(toObject(palio), { name: "Palio", brand: "Fiat", year: 2019 })
-assert.deepEqual(toObject(shelbyCobra), { name: "Shelby Cobra", brand: "Ford", year: 1963 })
-assert.deepEqual(toObject(chiron), { name: "Chiron", brand: "Bugatti", year: 2016 })
+assert.deepEqual(toObject(palio), { name: 'Palio', brand: 'Fiat', year: 2019 });
+assert.deepEqual(toObject(shelbyCobra), {
+  name: 'Shelby Cobra',
+  brand: 'Ford',
+  year: 1963,
+});
+assert.deepEqual(toObject(chiron), {
+  name: 'Chiron',
+  brand: 'Bugatti',
+  year: 2016,
+});
