65e41f28de8df087b111a2bf65ebdd6c7a6cce0b
Muniz <vinicius.muniz.marin@gmail.com>
2020-10-22 18:10:54 -0300

    exercise1

---
+++ modulo_1/bloco_9/dia_2/exercise1/apiScript.js
@@ -0,0 +1,18 @@
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const myObject = {
+  method: 'GET',
+  headers: { Accept: 'application/json' },
+};
+
+const jokePhrase = document.getElementById('jokeContainer');
+
+async function fetchJoke() {
+  const aJoke = await fetch(API_URL, myObject)
+    .then((response) => response.json())
+    .then((data) => data);
+  jokePhrase.innerText = aJoke.joke;
+  console.log(aJoke);
+}
+
+window.onload = () => fetchJoke();
