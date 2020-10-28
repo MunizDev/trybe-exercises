b6c2215a4c438b326440a2a15e1c31ed17224c74
Muniz <vinicius.muniz.marin@gmail.com>
2020-10-28 18:20:48 -0300

    exercise 1.2

---
+++ modulo_1/bloco_10/dia_1/__tests__/2.remove item array.test.js
@@ -0,0 +1,19 @@
+const myRemove = require('../functions/2.remove item array');
+
+test('a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+  expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+});
+
+test('a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+  expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+});
+
+test('o array passado por parâmetro não sofreu alterações', () => {
+  const array = [1, 2, 3, 4];
+  myRemove(array, 3);
+  expect(array).not.toEqual(myRemove(array, 3));
+});
+
+test('a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
