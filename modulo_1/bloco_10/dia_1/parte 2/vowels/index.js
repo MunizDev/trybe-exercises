c62dcab29442f7b3bf95711fcb5b7aa84c29a62a
Muniz <vinicius.muniz.marin@gmail.com>
2020-10-28 20:11:37 -0300

    exercise 2.4

--- modulo_1/bloco_10/dia_1/parte 2/vowels/index.js
+++ modulo_1/bloco_10/dia_1/parte 2/vowels/index.js
@@ -8,7 +8,28 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-
+  strArray = str.toLowerCase().split('');
+  let totalVowels = 0;
+  strArray.forEach((element) => {
+    if (element === 'a') {
+      totalVowels += 1;
+    }
+    if (element === 'e') {
+      totalVowels += 1;
+    }
+    if (element === 'i') {
+      totalVowels += 1;
+    }
+    if (element === 'o') {
+      totalVowels += 1;
+    }
+    if (element === 'u') {
+      totalVowels += 1;
+    }
+  });
+  return totalVowels;
 }
+
+console.log(vowels('Why do you ask?'));
 
 module.exports = vowels;
