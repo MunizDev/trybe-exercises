8e0e4c5e576fee6a7f9533f01e8f4f07e9cf6134
Muniz <vinicius.muniz.marin@gmail.com>
2020-11-17 21:34:31 -0300

    exercicio do dia

---
+++ modulo_2/bloco_13/dia_1/exercise-dog-image/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DogImages from './DogImages';
+
+function App() {
+  return (
+    <div className="App">
+      <DogImages />
+    </div>
+  );
+}
+
+export default App;
