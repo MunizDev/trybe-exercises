0e194f36852e3111777333b41b1a6a036b7261b4
Muniz <vinicius.muniz.marin@gmail.com>
2021-01-20 23:22:31 -0300

    exercises bloco 18 dia 3

---
+++ modulo_2/bloco_18/dia_3/exercise-two-two/src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+function Greeting({ initialName = '' }) {
+  // 🐨 inicialize o estado com o valor que vem do localStorage
+  // 💰 window.localStorage.getItem('name') || initialName
+  const storageName = window.localStorage.getItem('name');
+
+  const initialValue = storageName ? storageName : initialName;
+  const [name, setName] = useState(initialValue);
+
+  // 🐨 Utilize o hook useEffect para atualizar a
+  // propriedade `name` no localStorage quando o estado for alterado
+  // 💰 window.localStorage.setItem('name', name)
+
+  useEffect(() => {
+    window.localStorage.setItem('name', name);
+  }, [name]);
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  );
+}
+
+function App() {
+  return <Greeting />;
+}
+
+export default App;
