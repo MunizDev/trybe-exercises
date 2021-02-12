d8af62ef33959cc300dcfc002e49dd46644659d3
Muniz <vinicius.muniz.marin@gmail.com>
2021-02-11 21:26:37 -0300

    exercises modulo 3

---
+++ modulo_3/dia_2/15-exercises.sql
@@ -0,0 +1,16 @@
+SELECT "This is SQL Exercise, Practice and Solution";
+SELECT 1, 2, 3;
+SELECT 10 + 15;
+SELECT 10 * 30 - 5;
+USE Scientists;
+SELECT * from Scientists;
+SELECT Name as "Nome do Projeto", Hours as "Tempo de Trabalho" from Projects;
+SELECT Name from Scientists ORDER BY Name;
+SELECT Name from Projects ORDER BY Name DESC;
+SELECT CONCAT("O projeto ", Name, " precisou de ", Hours, " horas para ser concluído.") AS "Tempo para executar os projetos" from Projects;
+SELECT Name, Hours from Projects ORDER BY Hours DESC LIMIT 3;
+SELECT DISTINCT Project from AssignedTo;
+SELECT Name from Projects ORDER BY Hours DESC LIMIT 1;
+SELECT Name, Hours from Projects ORDER BY Hours LIMIT 1 OFFSET 1;
+SELECT * from Projects ORDER BY Hours LIMIT 5;
+SELECT CONCAT("Existem ", COUNT(*), " cientistas na tabela Scientists.") as "Quantidade de cientistas" from Scientists;
