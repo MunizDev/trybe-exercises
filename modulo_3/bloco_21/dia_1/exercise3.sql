3bff54c878ddfe081c06b85cf694cf90aba05543
Muniz <vinicius.muniz.marin@gmail.com>
2021-02-24 20:01:25 -0300

    exercises 3, 4 AND 5

--- modulo_3/bloco_21/dia_1/exercise3.sql
+++ modulo_3/bloco_21/dia_1/exercise3.sql
@@ -6,3 +6,4 @@ SELECT MAX(salary) FROM employees WHERE JOB_ID = 'IT_PROG';
 SELECT AVG(salary), COUNT(*) FROM employees WHERE DEPARTMENT_ID = 90;
 SELECT ROUND(MAX(salary),0), ROUND(MIN(salary),0), ROUND(SUM(salary),0), ROUND(AVG(salary),0) FROM employees;
 SELECT JOB_ID, COUNT(*) FROM employees GROUP BY JOB_ID;
+SELECT DEPARTMENT_ID, AVG(salary), COUNT(*) FROM employees GROUP BY DEPARTMENT_ID HAVING COUNT(*) > 10;
