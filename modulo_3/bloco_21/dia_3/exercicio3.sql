836b28956a1cf1cac8c55ed91091fcc8c0891fdf
munizdev <vinicius.muniz.marin@gmail.com>
2021-02-26 23:14:07 -0300

    ex3

---
+++ modulo_3/bloco_21/dia_3/exercicio3.sql
@@ -0,0 +1,13 @@
+USE BeeMovies;
+
+DELIMITER $$
+
+ CREATE TRIGGER deletarFilme
+ BEFORE DELETE ON movies
+ FOR EACH ROW
+BEGIN
+ INSERT INTO movies_logs(movie_id, action, log_date)
+ VALUES(OLD.movie_id, 'DELETE', NOW());
+END $$
+
+DELIMITER ;
