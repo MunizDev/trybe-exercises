5fb78bc17b98386572375cfd4f8f5964bb4291d6
munizdev <vinicius.muniz.marin@gmail.com>
2021-02-26 21:25:20 -0300

    ex1

---
+++ modulo_3/bloco_21/dia_3/exercicio1.sql
@@ -0,0 +1,22 @@
+USE BeeMovies;
+
+DELIMITER $$
+
+ CREATE TRIGGER insertReleaseYear
+ BEFORE INSERT ON movies
+ FOR EACH ROW
+BEGIN
+ SET NEW.release_year=YEAR(NOW());
+END $$
+
+ CREATE TRIGGER insertLog
+ AFTER INSERT ON movies
+ FOR EACH ROW
+BEGIN
+ INSERT INTO movies_logs(movie_id, action, log_date)
+ VALUES(NEW.movie_id, 'INSERT', NOW());
+END $$
+
+DELIMITER ;
+
+
